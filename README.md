d59eb66e1cd5a7fdfbe1604f379f6c8a73ce74bb
Esha Reddy <esha.reddy@warriorlife.net>
2019-01-10 09:31:01 -0800

    Changed Color of Select

--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 **Version 1.0.0**
 
 - Changed font and made PWA
+
+**Version 1.1..0**
+
+- Changed Select Color
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'v1.0.0';
+const CACHE_NAME = 'v1.1.0';
 
 var urlsToCache = [
 	'index.html',
